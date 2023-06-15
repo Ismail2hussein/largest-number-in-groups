3fbc90f6c86593aa4fff4736d7c07bdd4a74a38b
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 20:56:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// Good Luck 💪🏾
+function findLargestNumbers(arr) {
+    return arr.map(subArr => Math.max(...subArr));
+  }
+  console.log(findLargestNumbers([[7, 3, 8, 11], [54, 12, 43, 77], [16, 11, 8]])); // Output: [11, 77, 16]
+  console.log(findLargestNumbers([[22, 55, 12], [45, 66, 95], [22, 5, 11]])); // Output: [55, 95, 22]
+  
